153fb061702b996ac77c7816d56212437233a570
Анна <nkieru13@gmail.com>
2024-08-30 23:56:06 +0300

    Add viewsets and django_filters in INSTALLED_APPS

--- pereval/pereval_pj/pereval_app/views.py
+++ pereval/pereval_pj/pereval_app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
+import django_filters
 
-# Create your views here.
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+
+class CoordsViewSet(viewsets.ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class ImagesViewSet(viewsets.ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+
+class AddedViewSet(viewsets.ModelViewSet):
+    queryset = Added.objects.all()
+    serializer_class = AddedSerializer
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['user__email']
--- pereval/pereval_pj/pereval_pj/settings.py
+++ pereval/pereval_pj/pereval_pj/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pereval_app',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
