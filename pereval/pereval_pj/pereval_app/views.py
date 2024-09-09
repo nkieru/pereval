f2d1e26d6b48aed75ca1fdb30a448e7fb5e38a51
Анна <nkieru13@gmail.com>
2024-09-09 20:44:59 +0300

    Override the partial_update

--- pereval/pereval_pj/pereval_app/views.py
+++ pereval/pereval_pj/pereval_app/views.py
@@ -28,3 +28,30 @@ class AddedViewSet(viewsets.ModelViewSet):
     queryset = Added.objects.all()
     serializer_class = AddedSerializer
     filterset_fields = ['user__email',]
+
+    def partial_update(self, request, *args, **kwargs):
+        added = self.get_object()
+        if added.status == 'N':
+            serializer = AddedSerializer(added, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    {
+                        'state': '1',
+                        'message': 'Changes saved',
+                    }
+                )
+            else:
+                return Response(
+                    {
+                        'state': '0',
+                        'message': serializer.errors
+                    }
+                )
+        else:
+            return Response(
+                {
+                    'state': '0',
+                    'message': f'Changes rejected. Status: {added.get_status_display()}'
+                }
+            )
